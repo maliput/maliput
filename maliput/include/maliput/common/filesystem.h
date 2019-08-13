c50ed88b25da86e2dd33384b1d2094849b571cc6
John Shepherd <john@openrobotics.org>
2019-08-13 20:24:48 +0000

    Revert filesystem files

--- maliput/include/maliput/common/filesystem.h
+++ maliput/include/maliput/common/filesystem.h
@@ -1,3 +1,4 @@
+// clang-format off
 #pragma once
 
 #include <string>
@@ -5,47 +6,53 @@
 namespace maliput {
 namespace common {
 
-class Path {
- public:
-  Path() = default;
-  Path(std::string path_);
+class Path
+{
+public:
 
-  void set_path(const std::string& path_) noexcept;
-  void set_as_temp();
-  const std::string& get_path() const noexcept;
-  bool is_file() const noexcept;
-  bool is_directory() const noexcept;
-  bool is_absolute() const noexcept;
-  bool exists() const noexcept;
-  void append(const std::string& path);
+    Path() = default;
+    Path(std::string path_);
 
- private:
-  void normalize();
+    void set_path(const std::string& path_) noexcept;
+    void set_as_temp();
+    const std::string &get_path() const noexcept;
+    bool is_file() const noexcept;
+    bool is_directory() const noexcept;
+    bool is_absolute() const noexcept;
+    bool exists() const noexcept;
+    void append(const std::string& path);
 
-  std::string path;
+private:
+
+    void normalize();
+
+    std::string path;
 };
 
-class Filesystem {
- public:
-  /* Returns false if path is not a file or file doesn't exists.
-  Returns true otherwhise. */
-  static bool remove_file(const Path& path);
-  /* Returns false if path is not a directory or directory doesn't exists.
-  Returns true otherwise. */
-  static bool remove_directory(const Path& path);
-  /* Returns false if it couldn't create the directory. False otherwise.*/
-  static bool create_directory(const Path& path);
-  /* Returns current working directory */
-  static Path get_cwd();
-  /* Reads the whole file into a string. Returns false if file could not be
-  opened. True otherwise */
-  static bool read_as_string(const Path& path, std::string& read_to);
-  /* The same thing as mkdir -p <path> */
-  static bool create_directory_recursive(const Path& path);
-  /* Returns the environment variable path. Empty string if it doesn't
-  exists*/
-  static std::string get_env_path(const std::string& env_var);
+class Filesystem
+{
+public:
+
+    /* Returns false if path is not a file or file doesn't exists.
+    Returns true otherwhise. */
+    static bool remove_file(const Path& path);
+    /* Returns false if path is not a directory or directory doesn't exists.
+    Returns true otherwise. */
+    static bool remove_directory(const Path& path);
+    /* Returns false if it couldn't create the directory. False otherwise.*/
+    static bool create_directory(const Path& path);
+    /* Returns current working directory */
+    static Path get_cwd();
+    /* Reads the whole file into a string. Returns false if file could not be
+    opened. True otherwise */
+    static bool read_as_string(const Path &path, std::string &read_to);
+    /* The same thing as mkdir -p <path> */
+    static bool create_directory_recursive(const Path &path);
+    /* Returns the environment variable path. Empty string if it doesn't
+    exists*/
+    static std::string get_env_path(const std::string &env_var);
 };
 
-}  // namespace common
-}  // namespace maliput
+} // namespace common
+} // namespace maliput
+// clang-format on
