cd11438b340490b366cc5039dfe9cf3fd2560f7d
John Shepherd <john@openrobotics.org>
2019-08-20 17:42:52 +0000

    Remove check for clang format

--- maliput/tools/run_clang_format.sh
+++ maliput/tools/run_clang_format.sh
@@ -12,17 +12,6 @@ REPO_DIR=$SCRIPT_DIR/..
 
 export PATH=$PATH:/home/$USER/.local/bin
 
-check_program_installed() {
-  if ! [ -x "$(command -v $1)" ]; then
-    printf "Error: %s was not found in your system.\n" "$1"
-    printf "Please install it by running:\n\n"
-    printf "sudo apt install %s\n\n" "$1"
-    exit 1
-  fi
-}
-
-check_program_installed ament_clang_format
-
 declare -i CLANGFORMATFAILED=0
 
 pushd $REPO_DIR
