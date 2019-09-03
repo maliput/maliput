bb932e53885397bb2223395509e75bd94cac36e5
John <john@openrobotics.org>
2019-09-03 08:43:23 -0700

    Add cmake clang format (#113)
    * Add cmake clang format
    
    * Remove clang formatter script

--- maliput/tools/reformat_code.sh
+++ maliput/tools/reformat_code.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# This is a simple script to run the ament_clang_format against all of the code
-# in the repository.  ament_clang_format will reformat all of the code according
-# to the format described in the top-level .clang-format file in this
-# repository.  It is recommended to run this before opening any pull request.
+# This script runs clang-format and reformats any C++ code in 
+# the package. It expects a .clang-format to exist within the
+# root directory of the project.  To stop a directory from being
+# reformatted, add a blank `AMENT_IGNORE` file to the root of
+# the directory to exclude.
 
 SCRIPT_PATH=$(realpath ${BASH_SOURCE[0]})
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 REPO_DIR=$SCRIPT_DIR/..
 
+export PATH=$PATH:/home/$USER/.local/bin
+
+declare -i CLANGFORMATFAILED=0
+
 pushd $REPO_DIR
-./tools/run_clang_format.sh --reformat
+ament_clang_format --config=./../.clang-format --reformat || CLANGFORMATFAILED=1
 popd
+
+if [ "$CLANGFORMATFAILED" -ne "0" ]; then
+  echo $'\n*** ament_clang_format failed ***'
+  exit 1
+fi
