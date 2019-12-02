a2e489c0757db8e83a96b7a73f24f6adce8b740d
Franco <53065142+francocipollone@users.noreply.github.com>
2019-12-02 09:26:14 -0300

    Add function to obtain current bulb group. (#132)

--- maliput/include/maliput/base/rule_tools.h
+++ maliput/include/maliput/base/rule_tools.h
@@ -8,7 +8,7 @@
 namespace maliput {
 
 /// Returns a vector of Rule::Ids that represents the yield group of a Right-Of-Way rule state.
-/// @throws std::out_of_range When `discrete_value.related_rules` does not have `RightOfWayYieldGroup()` key.
+/// @throws common::assertion_error When `discrete_value.related_rules` does not have `RightOfWayYieldGroup()` key.
 std::vector<maliput::api::rules::Rule::Id> GetYieldGroup(
     const maliput::api::rules::DiscreteValueRule::DiscreteValue& discrete_value);
 
@@ -19,6 +19,21 @@ std::vector<maliput::api::rules::Rule::Id> GetYieldGroup(
 /// @throws common::assertion_error When `state_provider` is nullptr.
 /// @throws common::assertion_error When `state_provider` does not hold any state for `discrete_value_rule`.
 std::vector<maliput::api::rules::Rule::Id> GetCurrentYieldGroup(
+    const maliput::api::rules::DiscreteValueRule& discrete_value_rule,
+    const maliput::api::rules::DiscreteValueRuleStateProvider* state_provider);
+
+/// Returns a vector of maliput::api::UniqueIds that represents the bulb group of a Right-Of-Way rule state.
+/// @throws common::assertion_error When `discrete_value.related_unique_ids` does not have `RightOfWayBulbGroup()` key.
+std::vector<maliput::api::UniqueId> GetBulbGroup(
+    const maliput::api::rules::DiscreteValueRule::DiscreteValue& discrete_value);
+
+/// Returns the current bulb group of `discrete_value_rule`.
+/// @param discrete_value_rule A Right-Of-Way Rule Type rule.
+/// @param state_provider A state provider to retrieve current `discrete_value_rule` state. It must not be nullptr.
+/// @throws common::assertion_error When `discrete_value_rule.type_id()` is not RightOfWayRuleTypeId().
+/// @throws common::assertion_error When `state_provider` is nullptr.
+/// @throws common::assertion_error When `state_provider` does not hold any state for `discrete_value_rule`.
+std::vector<maliput::api::UniqueId> GetCurrentBulbGroup(
     const maliput::api::rules::DiscreteValueRule& discrete_value_rule,
     const maliput::api::rules::DiscreteValueRuleStateProvider* state_provider);
 
