c5ea771551f71e19f5db74f4fd36a583ab656d8a
Steven Peters <scpeters@openrobotics.org>
2020-01-14 18:05:07 +0100

    Replace std::not1 with std::not_fn (#234)

--- maliput/src/geometry_base/filter_positions.cc
+++ maliput/src/geometry_base/filter_positions.cc
@@ -1,6 +1,7 @@
 #include "maliput/geometry_base/filter_positions.h"
 
 #include <algorithm>
+#include <functional>
 
 namespace maliput {
 namespace geometry_base {
@@ -9,8 +10,7 @@ std::vector<api::RoadPositionResult> FilterRoadPositionResults(
     const std::vector<api::RoadPositionResult>& road_position_results,
     const std::function<bool(const api::RoadPositionResult&)>& filter) {
   std::vector<api::RoadPositionResult> result(road_position_results);
-  // TODO(agalbachicar)   If this code is ever moved to C++17, use std::not_fn() instead.
-  result.erase(std::remove_if(result.begin(), result.end(), std::not1(filter)), result.end());
+  result.erase(std::remove_if(result.begin(), result.end(), std::not_fn(filter)), result.end());
   return result;
 }
 
