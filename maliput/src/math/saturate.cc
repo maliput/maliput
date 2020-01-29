af746119341f9aa6e732f314e639ef00ec262d67
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2020-01-29 10:37:48 -0300

    Removes duplicated docstring.

--- maliput/src/math/saturate.cc
+++ maliput/src/math/saturate.cc
@@ -5,10 +5,6 @@
 namespace maliput {
 namespace math {
 
-/// When `x` is less than `min`, it returns `min`. When `x` is greater than `max`, it returns `max`.
-/// Otherwise, it returns `x`.
-///
-/// @throws common::assertion_error When `min` is greater than `max`.
 double saturate(double x, double min, double max) {
   MALIPUT_THROW_UNLESS(min <= max);
   if (x < min) return min;
