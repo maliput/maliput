5b9159afbe92c368b6a453274ef806319a9da9c5
Franco <53065142+francocipollone@users.noreply.github.com>
2020-02-27 09:11:26 -0300

    Fix math test.

--- maliput/test/math/math_test.py
+++ maliput/test/math/math_test.py
@@ -13,13 +13,13 @@ class TestMaliput(unittest.TestCase):
         Evaluates the constructor and accessors.
         '''
         kDut = Vector3(25., 158., 33.)
-        self.assertTrue(kDut.size(), 3.)
-        self.assertTrue(kDut.x(), 25.)
-        self.assertTrue(kDut.y(), 158.)
-        self.assertTrue(kDut.z(), 33.)
-        self.assertTrue(kDut[0], 25.)
-        self.assertTrue(kDut[1], 158.)
-        self.assertTrue(kDut[2], 33.)
+        self.assertTrue(kDut.size() == 3.)
+        self.assertTrue(kDut.x() == 25.)
+        self.assertTrue(kDut.y() == 158.)
+        self.assertTrue(kDut.z() == 33.)
+        self.assertTrue(kDut[0] == 25.)
+        self.assertTrue(kDut[1] == 158.)
+        self.assertTrue(kDut[2] == 33.)
         self.assertTrue(kDut == Vector3(25., 158., 33.))
         self.assertTrue(kDut != Vector3(33., 158., 25.))
 
@@ -28,15 +28,15 @@ class TestMaliput(unittest.TestCase):
         Evaluates the constructor and accessors.
         '''
         kDut = Vector4(25., 158., 33., 0.02)
-        self.assertTrue(kDut.size(), 4.)
-        self.assertTrue(kDut.x(), 25.)
-        self.assertTrue(kDut.y(), 158.)
-        self.assertTrue(kDut.z(), 33.)
-        self.assertTrue(kDut.w(), 0.02)
-        self.assertTrue(kDut[0], 25.)
-        self.assertTrue(kDut[1], 158.)
-        self.assertTrue(kDut[2], 33.)
-        self.assertTrue(kDut[3], 0.02)
+        self.assertTrue(kDut.size() == 4.)
+        self.assertTrue(kDut.x() == 25.)
+        self.assertTrue(kDut.y() == 158.)
+        self.assertTrue(kDut.z() == 33.)
+        self.assertTrue(kDut.w() == 0.02)
+        self.assertTrue(kDut[0] == 25.)
+        self.assertTrue(kDut[1] == 158.)
+        self.assertTrue(kDut[2] == 33.)
+        self.assertTrue(kDut[3] == 0.02)
         self.assertTrue(kDut == Vector4(25., 158., 33., 0.02))
         self.assertTrue(kDut != Vector4(0.02, 33., 158., 25.))
 
@@ -45,8 +45,8 @@ class TestMaliput(unittest.TestCase):
         Evaluates the constructor and accessors.
         '''
         kDut = Quaternion(0.884, 0.306, 0.177, 0.306)
-        self.assertTrue(kDut.w(), 0.884)
-        self.assertTrue(kDut.x(), 0.306)
-        self.assertTrue(kDut.y(), 0.177)
-        self.assertTrue(kDut.z(), 0.306)
+        self.assertTrue(kDut.w() == 0.884)
+        self.assertTrue(kDut.x() == 0.306)
+        self.assertTrue(kDut.y() == 0.177)
+        self.assertTrue(kDut.z() == 0.306)
         self.assertTrue(kDut.coeffs() == Vector4(0.884, 0.306, 0.177, 0.306))
