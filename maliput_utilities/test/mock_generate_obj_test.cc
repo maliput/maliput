28f137d2bbabff7df282b010a33b6b455c7bbf29
Steven Peters <scpeters@openrobotics.org>
2020-03-02 13:27:32 -0800

    add expected mtl/obj for mock test

--- maliput_utilities/test/mock_generate_obj_test.cc
+++ maliput_utilities/test/mock_generate_obj_test.cc
@@ -33,6 +33,10 @@ class MockGenerateObjTest : public ::testing::Test {
     ASSERT_TRUE(common::Filesystem::remove_directory(directory_));
   }
 
+  void ReadAsString(const common::Path& path, std::string* destination) {
+    ASSERT_TRUE(common::Filesystem::read_as_string(path, *destination)) << "failed to read " << path.get_path();
+  }
+
   common::Path directory_;
   std::vector<common::Path> paths_to_cleanup_;
 };
@@ -40,21 +44,34 @@ class MockGenerateObjTest : public ::testing::Test {
 TEST_F(MockGenerateObjTest, TwoLanesRoadGeometry) {
   const std::unique_ptr<const api::RoadGeometry> dut = api::test::CreateTwoLanesRoadGeometry();
 
+  const std::string basename{"TwoLanesRoadGeometry"};
+  std::string expected_obj_contents;
+  ReadAsString(basename + ".obj", &expected_obj_contents);
+  std::string expected_mtl_contents;
+  ReadAsString(basename + ".mtl", &expected_mtl_contents);
+
   ObjFeatures features;
   features.min_grid_resolution = 5.0;
-  const std::string basename{"TwoLanesRoadGeometry"};
   GenerateObjFile(dut.get(), directory_.get_path(), basename, features);
 
   common::Path actual_obj_path(directory_);
   actual_obj_path.append(basename + ".obj");
   EXPECT_TRUE(actual_obj_path.is_file());
+  paths_to_cleanup_.push_back(actual_obj_path);
 
   common::Path actual_mtl_path(directory_);
   actual_mtl_path.append(basename + ".mtl");
   EXPECT_TRUE(actual_mtl_path.is_file());
-
-  paths_to_cleanup_.push_back(actual_obj_path);
   paths_to_cleanup_.push_back(actual_mtl_path);
+
+  // Quick regression test on the OBJ and MTL.
+  std::string actual_obj_contents;
+  ReadAsString(actual_obj_path, &actual_obj_contents);
+  EXPECT_EQ(expected_obj_contents, actual_obj_contents);
+
+  std::string actual_mtl_contents;
+  ReadAsString(actual_mtl_path, &actual_mtl_contents);
+  EXPECT_EQ(expected_mtl_contents, actual_mtl_contents);
 }
 
 }  // namespace test
