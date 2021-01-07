d535d3aa38eef4ece9ca54304b4990e0c23ac8db
Franco Cipollone <franco.c@ekumenlabs.com>
2021-01-07 11:13:41 -0300

    Enhance maliput_plugin implementation.

--- maliput/include/maliput/plugin/maliput_plugin.h
+++ maliput/include/maliput/plugin/maliput_plugin.h
@@ -1,6 +1,8 @@
 // Copyright 2021 Toyota Research Institute
 #pragma once
 
+#include <functional>
+#include <memory>
 #include <string>
 
 #include <dlfcn.h>
@@ -14,9 +16,9 @@ namespace plugin {
 /// Persistent identifier for a MaliputPlugin element.
 using MaliputPluginId = api::TypeSpecificIdentifier<class MaliputPlugin>;
 
-/// MaliputPlugin is constructed from a library handler previously loaded.
+/// MaliputPlugin loads a dynamic library.
 /// After construction, the id and type of the plugin are provided.
-/// Besides, it helps to execute any symbol from the library.
+/// I adition this allows you to execute any symbol from the library.
 class MaliputPlugin {
  public:
   MALIPUT_NO_COPY_NO_MOVE_NO_ASSIGN(MaliputPlugin);
@@ -27,35 +29,38 @@ class MaliputPlugin {
   };
 
   /// Constructs a MaliputPlugin
-  /// @param lib_handle library pointer.
-  /// @throws maliput::common::assertion_error When `lib_handle` is nullptr.
-  MaliputPlugin(void* lib_handle);
+  /// @param path_to_lib Path to the library.
+  /// @throws maliput::common::assertion_error When `path_to_lib` is empty.
+  MaliputPlugin(const std::string& path_to_lib);
 
   MaliputPlugin() = delete;
 
   /// @returns The Id of the plugin.
-  std::string GetId() { return id_.string(); }
+  std::string GetId() const { return id_.string(); }
 
   /// @returns The Type of the plugin.
-  Type GetType() { return type_; }
+  Type GetType() const { return type_; }
 
   /// Finds and executes a symbol loaded by the plugin library.
   /// @tparam ReturnType The return type of the symbol that is executed.
+  /// @tparam Args Types of the symbol arguments.
+  ///
   /// @param sym_name Name of the symbol to execute.
+  /// @param args Argument list for the symbol call.
   /// @returns An object returned by the executed symbol.
   ///
   /// @throws maliput::common::assertion_error When `sym_name` is not found.
-  template <typename ReturnType>
-  ReturnType ExecuteSymbol(const std::string& sym_name) {
+  template <typename ReturnType, typename... Args>
+  ReturnType ExecuteSymbol(const std::string& sym_name, Args&&... args) {
+    // Reset error string.
     dlerror();
-    typedef ReturnType (*getter_t)();
-    getter_t getter = (getter_t)dlsym(lib_handle_, sym_name.c_str());
+    typedef ReturnType (*method_t)(Args...);
+    const method_t method = (method_t)dlsym(lib_handle_.get(), sym_name.c_str());
     const char* dlsym_error = dlerror();
     if (dlsym_error) {
-      const std::string msg{};
       MALIPUT_THROW_MESSAGE("Cannot load symbol " + sym_name + " : " + static_cast<std::string>(dlsym_error));
     }
-    return getter();
+    return method(std::forward<Args>(args)...);
   }
 
  private:
@@ -65,7 +70,7 @@ class MaliputPlugin {
   static constexpr char const* kMaliputPluginTypeSym{"GetMaliputPluginType"};
 
   /// Handle of the library.
-  void* lib_handle_;
+  std::unique_ptr<void, std::function<void(void*)>> lib_handle_;
   /// Id of the plugin.
   MaliputPluginId id_{"none"};
   /// Type of the plugin.
--- maliput/src/plugin/maliput_plugin.cc
+++ maliput/src/plugin/maliput_plugin.cc
@@ -4,9 +4,16 @@
 namespace maliput {
 namespace plugin {
 
-MaliputPlugin::MaliputPlugin(void* lib_handle) : lib_handle_(lib_handle) {
-  MALIPUT_THROW_UNLESS(lib_handle_ != nullptr);
-
+MaliputPlugin::MaliputPlugin(const std::string& path_to_lib) {
+  MALIPUT_THROW_UNLESS(!path_to_lib.empty());
+  // Call dlerror() before dlopen(~) to ensure that we get accurate error
+  // reporting afterwards.
+  dlerror();
+  lib_handle_ = std::unique_ptr<void, std::function<void(void*)>>(dlopen(path_to_lib.c_str(), RTLD_LAZY),
+                                                                  [](void* ptr) { dlclose(ptr); });
+  if (!lib_handle_) {
+    MALIPUT_THROW_MESSAGE("Cannot load library: " + static_cast<std::string>(dlerror()));
+  }
   id_ = MaliputPluginId(ExecuteSymbol<std::string>(kMaliputPluginIdSym));
   type_ = ExecuteSymbol<MaliputPlugin::Type>(kMaliputPluginTypeSym);
 }
