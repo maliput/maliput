2cf567427f633cd2ea9c9711c2856c0bc15ff9aa
Franco Cipollone <franco.c@ekumenlabs.com>
2021-01-14 22:04:58 -0300

    Adds test for MaliputPlugin.

---
+++ maliput/test/plugin/maliput_plugin_test.cc
@@ -0,0 +1,45 @@
+// Copyright 2021 Toyota Research Institute
+// @file
+// This test file is highly coupled to the maliput::dumb_plugin library creation.
+// By using the aforementioned library, maliput::plugin::MaliputPlugin class is tested.
+
+#include "maliput/plugin/maliput_plugin.h"
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "maliput/common/assertion_error.h"
+
+namespace maliput {
+namespace plugin {
+namespace {
+
+// MaliputPlugin's constructor receives an empty path.
+GTEST_TEST(MaliputPlugin, EmptyPath) {
+  const std::string kEmptyPath{""};
+  EXPECT_THROW(MaliputPlugin{kEmptyPath}, maliput::common::assertion_error);
+}
+
+// MaliputPlugin's constructor receives a wrong path.
+GTEST_TEST(MaliputPlugin, kWrongPath) {
+  const std::string kWrongPath{"/wrong/library/path/lib.so"};
+  EXPECT_THROW(MaliputPlugin{kWrongPath}, maliput::common::assertion_error);
+}
+
+// MaliputPlugin's methods are tested loading a correct library.
+GTEST_TEST(MaliputPlugin, FunctionsCall) {
+  const std::string kLibraryPath{"/tmp/maliput/test/plugins/libmaliput_dumb_plugin_1.so"};
+  const std::string kCustomSymbol{"MultiplyIntegers"};
+  const std::string kWrongSymbol{"WrongSymbol"};
+  const MaliputPlugin maliput_plugin{kLibraryPath};
+  EXPECT_EQ("dumb_plugin_1", maliput_plugin.GetId());
+  EXPECT_EQ(MaliputPluginType::kRoadNetworkLoader, maliput_plugin.GetType());
+
+  EXPECT_THROW(maliput_plugin.ExecuteSymbol<double>(kWrongSymbol), maliput::common::assertion_error);
+  EXPECT_EQ(200, maliput_plugin.ExecuteSymbol<int>(kCustomSymbol, 10, 20));
+}
+
+}  // namespace
+}  // namespace plugin
+}  // namespace maliput
