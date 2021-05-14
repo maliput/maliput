03db8fad5b93e3171967d227df79d5545307d803
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2021-05-14 12:45:04 -0300

    Improve error verbosity in Phases (#415)

--- maliput/src/api/rules/phase_ring.cc
+++ maliput/src/api/rules/phase_ring.cc
@@ -14,25 +14,33 @@ namespace {
 /// or Bulb::Id referenced in any one phase must be referenced in all phases.
 /// This is because every phase must specify the complete state of all the rules
 /// and bulb states mentioned by the ring.
-void VerifyAllPhasesHaveSameCoverage(const std::vector<Phase>& phases) {
-  MALIPUT_THROW_UNLESS(phases.size() >= 1);
+void VerifyAllPhasesHaveSameCoverage(const PhaseRing::Id& id, const std::vector<Phase>& phases) {
+  MALIPUT_VALIDATE(phases.size() >= 1, "PhaseRing(" + id.string() + ") must have at least one phase.");
   const auto& r = phases.at(0);  // The reference phase.
   for (const auto& phase : phases) {
-    MALIPUT_THROW_UNLESS(phase.rule_states().size() == r.rule_states().size());
+    MALIPUT_VALIDATE(phase.rule_states().size() == r.rule_states().size(),
+                     "PhaseRing(" + id.string() + "). Phase(" + phase.id().string() + ") and Phase(" + r.id().string() +
+                         ") must have the same number of rule_states.");
     for (const auto& s : phase.rule_states()) {
-      MALIPUT_THROW_UNLESS(r.rule_states().count(s.first) == 1);
+      MALIPUT_VALIDATE(r.rule_states().count(s.first) == 1, "PhaseRing(" + id.string() + "). Phase(" +
+                                                                phase.id().string() + ") has duplicated rule states.");
     }
-    MALIPUT_THROW_UNLESS(phase.discrete_value_rule_states().size() == r.discrete_value_rule_states().size());
+    MALIPUT_VALIDATE(phase.discrete_value_rule_states().size() == r.discrete_value_rule_states().size(),
+                     "PhaseRing(" + id.string() + "). Phase(" + phase.id().string() + ") and Phase(" + r.id().string() +
+                         ") must have the same number of discrete_value_rule_states.");
     for (const auto& s : phase.discrete_value_rule_states()) {
-      MALIPUT_THROW_UNLESS(r.discrete_value_rule_states().count(s.first) == 1);
+      MALIPUT_VALIDATE(r.discrete_value_rule_states().count(s.first) == 1,
+                       "PhaseRing(" + id.string() + "). Phase(" + phase.id().string() +
+                           ") has duplicated discrete value rule states");
     }
     // Require both set of bulb states to be defined or undefined together.
-    MALIPUT_THROW_UNLESS((r.bulb_states() == std::nullopt && phase.bulb_states() == std::nullopt) ||
-                         (r.bulb_states() != std::nullopt && phase.bulb_states() != std::nullopt));
     if (r.bulb_states() != std::nullopt) {
-      MALIPUT_THROW_UNLESS(phase.bulb_states()->size() == r.bulb_states()->size());
+      MALIPUT_VALIDATE(phase.bulb_states()->size() == r.bulb_states()->size(),
+                       "Phase(" + id.string() + "). Phase(" + phase.id().string() + ") and Phase(" + r.id().string() +
+                           ") must have the same number of bulb_states.");
       for (const auto& s : *phase.bulb_states()) {
-        MALIPUT_THROW_UNLESS(r.bulb_states()->count(s.first) == 1);
+        MALIPUT_VALIDATE(r.bulb_states()->count(s.first) == 1,
+                         "PhaseRing(" + id.string() + ") has duplicated bulb states");
       }
     }
   }
@@ -40,11 +48,14 @@ void VerifyAllPhasesHaveSameCoverage(const std::vector<Phase>& phases) {
 
 /// Tests that `next_phases` defines the possible next phases of every
 /// phase in `phases` and nothing more.
-void VerifyNextPhases(const std::vector<Phase>& phases,
+void VerifyNextPhases(const PhaseRing::Id& id, const std::vector<Phase>& phases,
                       const std::unordered_map<Phase::Id, std::vector<PhaseRing::NextPhase>>& next_phases) {
-  MALIPUT_THROW_UNLESS(phases.size() == next_phases.size());
+  MALIPUT_VALIDATE(phases.size() == next_phases.size(),
+                   "PhaseRing(" + id.string() + ")'s phases and next_phases sizes must be equal.");
   for (const auto& phase : phases) {
-    MALIPUT_THROW_UNLESS(next_phases.find(phase.id()) != next_phases.end());
+    MALIPUT_VALIDATE(
+        next_phases.find(phase.id()) != next_phases.end(),
+        "In PhaseRing(" + id.string() + "), the Phase(" + phase.id().string() + ") is not in next_phases.");
   }
 }
 
@@ -53,7 +64,7 @@ void VerifyNextPhases(const std::vector<Phase>& phases,
 PhaseRing::PhaseRing(const Id& id, const std::vector<Phase>& phases,
                      const std::optional<const std::unordered_map<Phase::Id, std::vector<NextPhase>>>& next_phases)
     : id_(id) {
-  MALIPUT_THROW_UNLESS(phases.size() >= 1);
+  MALIPUT_VALIDATE(phases.size() >= 1, "PhaseRing(" + id_.string() + ") must have at least one phase.");
   for (const Phase& phase : phases) {
     // Construct index of phases by ID, ensuring uniqueness of ID's.
     auto result = phases_.emplace(phase.id(), phase);
@@ -61,13 +72,13 @@ PhaseRing::PhaseRing(const Id& id, const std::vector<Phase>& phases,
   }
   if (next_phases != std::nullopt) {
     next_phases_ = *next_phases;
-    VerifyNextPhases(phases, next_phases_);
+    VerifyNextPhases(id_, phases, next_phases_);
   } else {
     for (const auto& phase : phases) {
       next_phases_.emplace(std::make_pair(phase.id(), std::vector<NextPhase>()));
     }
   }
-  VerifyAllPhasesHaveSameCoverage(phases);
+  VerifyAllPhasesHaveSameCoverage(id_, phases);
 }
 
 std::optional<Phase> PhaseRing::GetPhase(const Phase::Id& id) const {
