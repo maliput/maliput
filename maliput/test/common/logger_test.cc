3532c679770ff35adc23b658d1a44a86dde1efb7
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2021-06-17 16:47:49 -0300

    Allows the logger to escape fmt formatting. (#427)

--- maliput/test/common/logger_test.cc
+++ maliput/test/common/logger_test.cc
@@ -23,7 +23,7 @@ class MockSink : public SinkBase {
   ~MockSink() = default;
 
   const std::string& get_log_message() const { return log_message_; }
-  void log(const std::string& msg) override { log_message_ = fmt::format(msg); }
+  void log(const std::string& msg) override { log_message_ = msg; }
   void flush() override{};
 
  private:
@@ -40,47 +40,6 @@ GTEST_TEST(LoggerTest, SetSink) {
   EXPECT_THROW(log()->set_sink(std::unique_ptr<SinkBase>()), common::assertion_error);
 }
 
-GTEST_TEST(LoggerTest, Logger) {
-  std::unique_ptr<MockSink> mock_sink = std::make_unique<MockSink>();
-  const MockSink* mock_sink_ptr = mock_sink.get();
-  log()->set_sink(std::move(mock_sink));
-
-  const std::string kMessage1 = " Hello World. {}{}.";
-  const std::string kMessage2 = "The value of PI is: ";
-  const double kPI = 3.14159;
-  set_log_level(logger::kLevelToString.at(logger::level::trace));
-  {
-    const std::string kExpectedMessage{"[TRACE]  Hello World. The value of PI is: 3.14159.\n"};
-    log()->trace(kMessage1, kMessage2, kPI);
-    EXPECT_EQ(mock_sink_ptr->get_log_message(), kExpectedMessage);
-  }
-  {
-    const std::string kExpectedMessage{"[DEBUG]  Hello World. The value of PI is: 3.14159.\n"};
-    log()->debug(kMessage1, kMessage2, kPI);
-    EXPECT_EQ(mock_sink_ptr->get_log_message(), kExpectedMessage);
-  }
-  {
-    const std::string kExpectedMessage{"[INFO]  Hello World. The value of PI is: 3.14159.\n"};
-    log()->info(kMessage1, kMessage2, kPI);
-    EXPECT_EQ(mock_sink_ptr->get_log_message(), kExpectedMessage);
-  }
-  {
-    const std::string kExpectedMessage{"[WARNING]  Hello World. The value of PI is: 3.14159.\n"};
-    log()->warn(kMessage1, kMessage2, kPI);
-    EXPECT_EQ(mock_sink_ptr->get_log_message(), kExpectedMessage);
-  }
-  {
-    const std::string kExpectedMessage{"[ERROR]  Hello World. The value of PI is: 3.14159.\n"};
-    log()->error(kMessage1, kMessage2, kPI);
-    EXPECT_EQ(mock_sink_ptr->get_log_message(), kExpectedMessage);
-  }
-  {
-    const std::string kExpectedMessage{"[CRITICAL]  Hello World. The value of PI is: 3.14159.\n"};
-    log()->critical(kMessage1, kMessage2, kPI);
-    EXPECT_EQ(mock_sink_ptr->get_log_message(), kExpectedMessage);
-  }
-}
-
 GTEST_TEST(LoggerTest, SetLogLevel) {
   std::unique_ptr<MockSink> mock_sink = std::make_unique<MockSink>();
   MockSink* mock_sink_ptr = mock_sink.get();
@@ -93,6 +52,67 @@ GTEST_TEST(LoggerTest, SetLogLevel) {
   log()->error("Hello World");
   EXPECT_EQ(mock_sink_ptr->get_log_message(), std::string{});
   EXPECT_THROW(set_log_level("wrong_level"), std::out_of_range);
+}
+
+class LoggingMesagesTest : public ::testing::Test {
+ protected:
+  static constexpr char const* kMessage1 = " Hello World. {}{}.";
+  static constexpr char const* kMessage2 = "The value of PI is: ";
+  static constexpr char const* kExpectedMessage = "  Hello World. The value of PI is: 3.14159.\n";
+  static constexpr double kPI = 3.14159;
+
+  void SetUp() override {
+    mock_sink_ptr = mock_sink.get();
+    log()->set_sink(std::move(mock_sink));
+    set_log_level(logger::kLevelToString.at(logger::level::trace));
+  }
+
+  std::unique_ptr<MockSink> mock_sink = std::make_unique<MockSink>();
+  MockSink* mock_sink_ptr{nullptr};
+};
+
+TEST_F(LoggingMesagesTest, EscapingFormatting) {
+  // Double curly braces escapes fmt formating.
+  const std::string message{"Escape {{formatting}}"};
+  const std::string expected_message{"[TRACE] Escape {formatting}\n"};
+  log()->trace(message);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
+}
+
+TEST_F(LoggingMesagesTest, TraceLog) {
+  const std::string expected_message{"[TRACE]" + std::string(kExpectedMessage)};
+  log()->trace(kMessage1, kMessage2, kPI);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
+}
+
+TEST_F(LoggingMesagesTest, DebugLog) {
+  const std::string expected_message{"[DEBUG]" + std::string(kExpectedMessage)};
+  log()->debug(kMessage1, kMessage2, kPI);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
+}
+
+TEST_F(LoggingMesagesTest, InfoLog) {
+  const std::string expected_message{"[INFO]" + std::string(kExpectedMessage)};
+  log()->info(kMessage1, kMessage2, kPI);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
+}
+
+TEST_F(LoggingMesagesTest, WarningLog) {
+  const std::string expected_message{"[WARNING]" + std::string(kExpectedMessage)};
+  log()->warn(kMessage1, kMessage2, kPI);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
+}
+
+TEST_F(LoggingMesagesTest, ErrorLog) {
+  const std::string expected_message{"[ERROR]" + std::string(kExpectedMessage)};
+  log()->error(kMessage1, kMessage2, kPI);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
+}
+
+TEST_F(LoggingMesagesTest, CriticalLog) {
+  const std::string expected_message{"[CRITICAL]" + std::string(kExpectedMessage)};
+  log()->critical(kMessage1, kMessage2, kPI);
+  EXPECT_EQ(mock_sink_ptr->get_log_message(), expected_message);
 }
 
 }  // namespace
