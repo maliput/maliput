f00111f3da84ef2847600c4aae69383e83a74685
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2022-02-15 11:18:40 -0300

    Adds phase ring book loader for new rules: (#464)
    * Move old rules PhaseRingBook loader methods to be <method_name>OldRules()
    * Create PhaseRingBook loader for new rules.

--- maliput/include/maliput/base/phase_ring_book_loader.h
+++ maliput/include/maliput/base/phase_ring_book_loader.h
@@ -10,6 +10,34 @@
 namespace maliput {
 
 /// Instantiates and returns an api::rules::PhaseRingBook instance based on the
+/// specified @p rulebook, and @p input document. Based on old rule API.
+///
+/// @param rulebook Contains the rules.
+///
+/// @param traffic_light_book Contains the traffic lights.
+///
+/// @param input The YAML PhaseRings document.
+///
+/// @return The newly created api::rules::PhaseRingBook instance.
+std::unique_ptr<api::rules::PhaseRingBook> LoadPhaseRingBookOldRules(
+    const api::rules::RoadRulebook* rulebook, const api::rules::TrafficLightBook* traffic_light_book,
+    const std::string& input);
+
+/// Instantiates and returns an api::rules::PhaseRingBook instance based on the
+/// specified @p rulebook, and @p filename. Based on old rule API.
+///
+/// @param rulebook Contains the rules.
+///
+/// @param traffic_light_book Contains the traffic lights.
+///
+/// @param filename The YAML file that contains a PhaseRings document.
+///
+/// @return The newly created api::rules::PhaseRingBook instance.
+std::unique_ptr<api::rules::PhaseRingBook> LoadPhaseRingBookFromFileOldRules(
+    const api::rules::RoadRulebook* rulebook, const api::rules::TrafficLightBook* traffic_light_book,
+    const std::string& filename);
+
+/// Instantiates and returns an api::rules::PhaseRingBook instance based on the
 /// specified @p rulebook, and @p input document.
 ///
 /// @param rulebook Contains the rules.
