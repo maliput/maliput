ff08eea3f88c4b8a755718949b022243d3aea0dd
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2022-09-26 08:33:49 -0300

    Adds IsLanePositionResultClose macro. (#522)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- include/maliput/test_utilities/maliput_types_compare.h
+++ include/maliput/test_utilities/maliput_types_compare.h
@@ -37,57 +37,64 @@ namespace maliput {
 namespace api {
 namespace test {
 
-// Compares equality within @p tolerance deviation of two InertialPosition objects.
-// @param pos1 A InertialPosition object to compare.
-// @param pos2 A InertialPosition object to compare.
-// @param tolerance An allowable absolute deviation for each InertialPosition's
-// coordinate.
-// @return ::testing::AssertionFailure() When InertialPosition objects are different.
-// @return ::testing::AssertionSuccess() When InertialPosition objects are within
-// the @p tolerance deviation.
+/// Compares equality within @p tolerance deviation of the maliput::api::LanePositionResult @p lpr_a and @p lpr_B.
+/// @param lpr_a The first LanePositionResult to compare.
+/// @param lpr_b The second LanePositionResult to compare.
+/// @param tolerance The tolerance to use for the comparison.
+::testing::AssertionResult IsLanePositionResultClose(const LanePositionResult& lpr_a, const LanePositionResult& lpr_b,
+                                                     double tolerance);
+
+/// Compares equality within @p tolerance deviation of two InertialPosition objects.
+/// @param pos1 A InertialPosition object to compare.
+/// @param pos2 A InertialPosition object to compare.
+/// @param tolerance An allowable absolute deviation for each InertialPosition's
+/// coordinate.
+/// @return ::testing::AssertionFailure() When InertialPosition objects are different.
+/// @return ::testing::AssertionSuccess() When InertialPosition objects are within
+/// the @p tolerance deviation.
 ::testing::AssertionResult IsInertialPositionClose(const InertialPosition& pos1, const InertialPosition& pos2,
                                                    double tolerance);
 
-// Compares equality within @p tolerance deviation of two LanePosition objects.
-// @param pos1 A LanePosition object to compare.
-// @param pos2 A LanePosition object to compare.
-// @param tolerance An allowable absolute deviation for each LanePosition's
-// coordinate.
-// @return ::testing::AssertionFailure() When LanePosition objects are
-// different.
-// @return ::testing::AssertionSuccess() When LanePosition objects are equal or
-// within the @p tolerance deviation.
+/// Compares equality within @p tolerance deviation of two LanePosition objects.
+/// @param pos1 A LanePosition object to compare.
+/// @param pos2 A LanePosition object to compare.
+/// @param tolerance An allowable absolute deviation for each LanePosition's
+/// coordinate.
+/// @return ::testing::AssertionFailure() When LanePosition objects are
+/// different.
+/// @return ::testing::AssertionSuccess() When LanePosition objects are equal or
+/// within the @p tolerance deviation.
 ::testing::AssertionResult IsLanePositionClose(const LanePosition& pos1, const LanePosition& pos2, double tolerance);
 
-// Compares equality within @p tolerance deviation of two Rotation objects.
-// Comparison will evaluate the inner Rotation's Euler angles.
-// @param rot1 A Rotation object to compare.
-// @param rot2 A Rotation object to compare.
-// @param tolerance An allowable absolute deviation for each Rotation's
-// coordinate.
-// @return ::testing::AssertionFailure() When Rotation objects are different.
-// @return ::testing::AssertionSuccess() When Rotation objects are equal or
-// within the @p tolerance deviation.
+/// Compares equality within @p tolerance deviation of two Rotation objects.
+/// Comparison will evaluate the inner Rotation's Euler angles.
+/// @param rot1 A Rotation object to compare.
+/// @param rot2 A Rotation object to compare.
+/// @param tolerance An allowable absolute deviation for each Rotation's
+/// coordinate.
+/// @return ::testing::AssertionFailure() When Rotation objects are different.
+/// @return ::testing::AssertionSuccess() When Rotation objects are equal or
+/// within the @p tolerance deviation.
 ::testing::AssertionResult IsRotationClose(const Rotation& rot1, const Rotation& rot2, double tolerance);
 
-// Compares equality within @p tolerance deviation of two RBounds objects.
-// @param rbounds1 A RBounds object to compare.
-// @param rbounds2 A RBounds object to compare.
-// @param tolerance An allowable absolute deviation for each RBounds's instance
-// value.
-// @return ::testing::AssertionFailure() When RBounds objects are different.
-// @return ::testing::AssertionSuccess() When RBounds objects are equal or
-// within the @p tolerance deviation.
+/// Compares equality within @p tolerance deviation of two RBounds objects.
+/// @param rbounds1 A RBounds object to compare.
+/// @param rbounds2 A RBounds object to compare.
+/// @param tolerance An allowable absolute deviation for each RBounds's instance
+/// value.
+/// @return ::testing::AssertionFailure() When RBounds objects are different.
+/// @return ::testing::AssertionSuccess() When RBounds objects are equal or
+/// within the @p tolerance deviation.
 ::testing::AssertionResult IsRBoundsClose(const RBounds& rbounds1, const RBounds& rbounds2, double tolerance);
 
-// Compares equality within @p tolerance deviation of two HBounds objects.
-// @param hbounds1 A HBounds object to compare.
-// @param hbounds1 A HBounds object to compare.
-// @param tolerance An allowable absolute deviation for each HBounds's instance
-// value.
-// @return ::testing::AssertionFailure() When HBounds objects are different.
-// @return ::testing::AssertionSuccess() When HBounds objects are equal or
-// within the @p tolerance deviation.
+/// Compares equality within @p tolerance deviation of two HBounds objects.
+/// @param hbounds1 A HBounds object to compare.
+/// @param hbounds1 A HBounds object to compare.
+/// @param tolerance An allowable absolute deviation for each HBounds's instance
+/// value.
+/// @return ::testing::AssertionFailure() When HBounds objects are different.
+/// @return ::testing::AssertionSuccess() When HBounds objects are equal or
+/// within the @p tolerance deviation.
 ::testing::AssertionResult IsHBoundsClose(const HBounds& hbounds1, const HBounds& hbounds2, double tolerance);
 
 }  // namespace test
