9524be2c0abec60bf22423e4efc5e64455f15996
Santiago Lopez <santiago.lopez@ekumenlabs.com>
2025-09-03 11:36:05 -0300

    Range validator error type template.
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>

--- include/maliput/common/range_validator.h
+++ include/maliput/common/range_validator.h
@@ -29,7 +29,12 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
+#include <algorithm>
+#include <string>
+
+#include "maliput/common/error.h"
 #include "maliput/common/maliput_copyable.h"
+#include "maliput/common/maliput_throw.h"
 
 namespace maliput {
 namespace common {
@@ -41,6 +46,7 @@ namespace common {
 /// adapts it to be in the interval (min, max) where the difference between the
 /// close and open range is epsilon.
 /// This functor is motivated to wrap MALIPUT_VALIDATE() calls plus a std::clamp() to avoid throws for numerical errors.
+template <typename ErrorType = maliput::common::assertion_error>
 class RangeValidator {
  public:
   MALIPUT_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(RangeValidator)
@@ -52,13 +58,17 @@ class RangeValidator {
   /// @param tolerance is the range extension to be accepted.
   /// @param epsilon is the relative minimum difference that separates a number within
   ///                range to be distinct from extremes. It is relative to the range.
+  /// @tparam ErrorType The error type to be thrown.
   /// @returns A RangeValidator instance.
-  /// @throws maliput::common::assertion_error When `tolerance` is non positive.
-  /// @throws maliput::common::assertion_error When `epsilon` is not in
+  /// @throws ErrorType When `tolerance` is non positive.
+  /// @throws ErrorType When `epsilon` is not in
   ///         [0, tolerance].
-  /// @throws maliput::common::assertion_error When `min` + `epsilon` > `max` or
+  /// @throws ErrorType When `min` + `epsilon` > `max` or
   ///         `max` - `epsilon` < `min`
-  static RangeValidator GetRelativeEpsilonValidator(double min, double max, double tolerance, double epsilon);
+  static RangeValidator<ErrorType> GetRelativeEpsilonValidator(double min, double max, double tolerance,
+                                                               double epsilon) {
+    return RangeValidator<ErrorType>{min, max, tolerance, epsilon, EpsilonUse::kRelative};
+  }
 
   /// Creates a RangeValidator instance that uses a `epsilon` as the absolute epsilon to validate the values.
   /// @param min lower extreme of the range.
@@ -66,22 +76,33 @@ class RangeValidator {
   /// @param tolerance is the range extension to be accepted.
   /// @param epsilon is minimum difference that separates a number within the
   ///        range to be distinct from range extremes (`min` and `max`).
+  /// @tparam ErrorType The error type to be thrown.
   /// @returns A RangeValidator instance.
-  /// @throws maliput::common::assertion_error When `tolerance` is non positive.
-  /// @throws maliput::common::assertion_error When `epsilon` is not in
+  /// @throws ErrorType When `tolerance` is non positive.
+  /// @throws ErrorType When `epsilon` is not in
   ///         [0, tolerance].
-  /// @throws maliput::common::assertion_error When `min` + `epsilon` > `max` or
+  /// @throws ErrorType When `min` + `epsilon` > `max` or
   ///         `max` - `epsilon` < `min`
-  static RangeValidator GetAbsoluteEpsilonValidator(double min, double max, double tolerance, double epsilon);
+  static RangeValidator<ErrorType> GetAbsoluteEpsilonValidator(double min, double max, double tolerance,
+                                                               double epsilon) {
+    return RangeValidator<ErrorType>{min, max, tolerance, epsilon, EpsilonUse::kAbsolute};
+  }
 
   RangeValidator() = delete;
 
   /// Evaluates whether `s` is in range or not.
+  /// @param s value to check if it is in range.
   /// @returns `s` when it is within the open range. If `s` is equal to either
   /// range extremes or the difference to them is less or equal to tolerance, it
-  /// returns the closest open range value. Otherwise, it
-  /// @throws maliput::common::assertion_error.
-  double operator()(double s) const;
+  /// returns the closest open range value.
+  /// @throws ErrorType if @p s is out of range.
+  double operator()(double s) const {
+    MALIPUT_VALIDATE(s >= min_ - tolerance_, std::to_string(s) + " is less than " + std::to_string(min_ - tolerance_),
+                     ErrorType);
+    MALIPUT_VALIDATE(s <= max_ + tolerance_,
+                     std::to_string(s) + " is greater than " + std::to_string(max_ + tolerance_), ErrorType);
+    return std::clamp(s, min_ + epsilon_, max_ - epsilon_);
+  }
 
  private:
   // The provided epsilon can be used relatively or absolutely.
@@ -95,12 +116,33 @@ class RangeValidator {
   // @param epsilon is minimum difference that separates a number within the
   //        range to be distinct from range extremes (`min` and `max`).
   // @param epsilon_mode selects how `epsilon` will be used: absolute or relative.
-  // @throws maliput::common::assertion_error When `tolerance` is non positive.
-  // @throws maliput::common::assertion_error When `epsilon` is not in
+  // @throws ErrorType When `tolerance` is non positive.
+  // @throws ErrorType When `epsilon` is not in
   //         [0, tolerance].
-  // @throws maliput::common::assertion_error When `min` + `epsilon` > `max` or
+  // @throws ErrorType When `min` + `epsilon` > `max` or
   //         `max` - `epsilon` < `min`
-  RangeValidator(double min, double max, double tolerance, double epsilon, const EpsilonUse& epsilon_mode);
+  RangeValidator(double min, double max, double tolerance, double epsilon, const EpsilonUse& epsilon_mode)
+      : min_(min), max_(max), tolerance_(tolerance), epsilon_(epsilon) {
+    MALIPUT_THROW_UNLESS(tolerance_ > 0., ErrorType);
+
+    if (epsilon_mode == EpsilonUse::kRelative) {
+      // Multiplying the range by kEpsilon creates an epsilon value that is relative to the
+      // length of the range. This avoids numerical errors when having a long range and a very small epsilon value
+      // (~1e-14) leads to calculation that goes beyond the minimal useful digit of the double type.
+      epsilon_ = (max_ - min_) * epsilon_;
+    }
+    MALIPUT_VALIDATE(epsilon_ >= 0., std::to_string(epsilon_) + " is less than " + std::to_string(0.), ErrorType);
+    MALIPUT_VALIDATE(epsilon_ <= tolerance_,
+                     std::to_string(epsilon_) + " is greater than " + std::to_string(tolerance_), ErrorType);
+    MALIPUT_VALIDATE((min_ + epsilon_) <= max_,
+                     std::string("Open range lower bound <") + std::to_string((min_ + epsilon_)) +
+                         "> is greater than <" + std::to_string(max_) + ">",
+                     ErrorType);
+    MALIPUT_VALIDATE(min <= (max_ - epsilon_),
+                     std::string("Open range upper bound <") + std::to_string((max_ - epsilon_)) + "> is less than <" +
+                         std::to_string(min) + ">",
+                     ErrorType);
+  }
 
   double min_{};
   double max_{};
